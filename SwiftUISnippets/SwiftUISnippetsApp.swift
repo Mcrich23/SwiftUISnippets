aba17495bbcda66ad2b0191d1d2f9680d601fad4
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-12-10 13:05:51 -0800

    Initial Commit

---
+++ SwiftUISnippets/SwiftUISnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISnippetsApp.swift
+//  SwiftUISnippets
+//
+//  Created by Morris Richman on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
